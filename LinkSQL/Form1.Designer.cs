2bc099949f24fa7b1f1a69834252dc58f34ca52b
Zhoukehan <1298516531@qq.com>
2022-05-28 11:28:48 +0800

    添加项目文件。

---
+++ LinkSQL/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace LinkSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(405, 253);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "增加";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "删除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(499, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "修改";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(654, 331);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "查询";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "s_id";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(601, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 25);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(601, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "c_id";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(601, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 25);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "s_score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LinkSQL/Form1.cs
@@ -0,0 +1,148 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connString;
+        MySqlCommand comm = new MySqlCommand();
+        MySqlConnection conn;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connString = "server=localhost; database=test; uid=root; pwd=q1298516531;Character Set=utf8;";
+            conn = new MySqlConnection(connString);
+            comm.Connection = conn;
+            conn.Open();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "select * from score where ";
+                int n = 0;
+                if (textBox1.Text != "")
+                { 
+                    sql += label1.Text + " = " + textBox1.Text; 
+                    n++;
+                }
+                if (textBox2.Text != "")
+                {
+                    if (n == 0)
+                        sql += " " + label2.Text + " = " + textBox2.Text;
+                    else
+                        sql += " and " + label2.Text + " = " + textBox2.Text;
+                }
+                if (textBox4.Text != "")
+                {
+                    if (n == 0)
+                        sql += " " + label4.Text + " = " + textBox4.Text;
+                    else
+                        sql += " and " + label4.Text + " = " + textBox4.Text;
+                }
+                sql += ";";
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, connString);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "score");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "score";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作数据库出错！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "insert into score values("+textBox1.Text+","+textBox2.Text+","+textBox4.Text+");";
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, connString);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "score");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "score";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作数据库出错！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "delete from score where " ;
+                int n = 0;
+                if (textBox1.Text != "")
+                {
+                    sql += label1.Text + " = " + textBox1.Text;
+                    n++;
+                }
+                if (textBox2.Text != "")
+                {
+                    if (n == 0)
+                        sql += " " + label2.Text + " = " + textBox2.Text;
+                    else
+                        sql += " and " + label2.Text + " = " + textBox2.Text;
+                }
+                if (textBox4.Text != "")
+                {
+                    if (n == 0)
+                        sql += " " + label4.Text + " = " + textBox4.Text;
+                    else
+                        sql += " and " + label4.Text + " = " + textBox4.Text;
+                }
+                sql += ";";
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, connString);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "score");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "score";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作数据库出错！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = 0;
+                string sql = "update score set " + label4.Text + " = " + textBox4.Text + " where s_id = " + textBox1.Text + " and c_id=" + textBox4.Text + ";";
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, connString);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "score");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "score";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作数据库出错！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
